97c6764455e7b8590149d15e3a3776c8538da01e
Eugene Marin <em88il@gmail.com>
2023-01-13 23:08:14 +0200

    Environment refactor

--- src/main/java/lab/drop/runtime/Environment.java
+++ src/main/java/lab/drop/runtime/Environment.java
@@ -1,11 +1,60 @@
 package lab.drop.runtime;
 
+import java.lang.management.ManagementFactory;
+
 /**
- * Common environment properties.
+ * Common environment information.
  */
-public interface Environment {
-    String userName = System.getProperty("user.name");
-    String userHome = System.getProperty("user.home");
-    String workingDir = System.getProperty("user.dir");
-    String tempDir = System.getProperty("java.io.tmpdir");
+public abstract class Environment {
+
+    private Environment() {}
+
+    /**
+     * Returns the operating system name system property.
+     */
+    public static String os() {
+        return System.getProperty("os.name");
+    }
+
+    /**
+     * Returns the user name system property.
+     */
+    public static String userName() {
+        return System.getProperty("user.name");
+    }
+
+    /**
+     * Returns the user home directory path system property.
+     */
+    public static String userHome() {
+        return System.getProperty("user.home");
+    }
+
+    /**
+     * Returns the process working directory path system property.
+     */
+    public static String workingDir() {
+        return System.getProperty("user.dir");
+    }
+
+    /**
+     * Returns the temporary files directory path system property.
+     */
+    public static String tempDir() {
+        return System.getProperty("java.io.tmpdir");
+    }
+
+    /**
+     * Returns true if a debugger is defined on the JVM.
+     */
+    public static boolean isDebuggerPresent() {
+        return ManagementFactory.getRuntimeMXBean().getInputArguments().stream().anyMatch(arg -> arg.contains("jdwp"));
+    }
+
+    /**
+     * Returns true if current operating system is Windows.
+     */
+    public static boolean isWindows() {
+        return os().startsWith("Windows");
+    }
 }
